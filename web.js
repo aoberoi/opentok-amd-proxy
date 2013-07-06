0648b9b9be9b61bda6ab1b02f3622bc72585982a
Ankur Oberoi <aoberoi@gmail.com>
2013-07-06 11:20:43 -0400

    adding the real amd shim prefix and suffix

--- web.js
+++ web.js
@@ -2,10 +2,13 @@ var http = require('http');
 var ShimStream = require('./lib/shim-stream');
 
 var server = http.createServer(function(req, res) {
+
   // create a shim stream
   var ss = new ShimStream();
 
+  // get the remote javascript
   http.get('http://static.opentok.com/v1.1/js/TB.min.js', function(remoteRes) {
+    // pipe the transformed javascript back as the response
     remoteRes.pipe(ss).pipe(res);
   });
 });
